1f8a619c702e55e12150a7efc13c113e1cae93c9
The Almighty Grant <agsherrick@gmail.com>
2013-11-25 23:27:06 -0500

    Added a fab file for automating deployment and fixed other issues

---
+++ CrowdedMountain/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import local, lcd
+
+def prepare_deployment(branch_name):
+    local('python manage.py test CrowdedMountain')
+    local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
+
+
+#def deploy():
+#    with lcd('/path/to/my/prod/area/'):
+#        local('git pull /my/path/to/dev/area/')
+#        local('python manage.py migrate myapp')
+#        local('python manage.py test myapp')
+#        local('/my/command/to/restart/webserver')
