9620cc4c735ebe8dd0937824341d47b5d2464a37
Grant Sherrick <agsherrick@gmail.com>
2013-11-27 10:38:30 -0700

    minor changes for basic template

--- CrowdedMountain/apps/cdot_counting/views.py
+++ CrowdedMountain/apps/cdot_counting/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.template.loader import get_template
+from django.template import Template, Context, RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+#from CrowdedMountain.apps.cdot_counting.models import Resort
+import datetime
+
+#each view takes at least one parameter called a request
+#  The request contains information about the current
+#     web request that has triggered the view
+#  It is an instance of the class django.http.HttpRequest
+#  A view is just a Python function that takes an HttpRequest
+#    as its first paramter and returns an instance of an HttpResponse
+
+def ugly_count(request):
+    breck = 0
+    vail = 0
+    t = get_template('cdot_template.html')
+    html = t.render(Context({'breck': breck, 'vail': vail}))
+    return HttpResponse(html)
+
--- CrowdedMountain/urls.py
+++ CrowdedMountain/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^cdot_count$', 'CrowdedMountain.apps.cdot_counting.views.ugly_count'),
     # Examples:
     # url(r'^$', 'CrowdedMountain.views.home', name='home'),
     # url(r'^CrowdedMountain/', include('CrowdedMountain.foo.urls')),
@@ -13,5 +12,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
