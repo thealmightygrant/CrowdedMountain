7d0c6eaf80324ccb510bc65d6a3a00d05a14f1d3
Grant Sherrick <agsherrick@gmail.com>
2014-03-15 14:13:16 -0600

    minor change to functional tests

--- CrowdedMountain/tests/functional_tests.py
+++ CrowdedMountain/tests/functional_tests.py
@@ -5,6 +5,7 @@ class NewVistorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
