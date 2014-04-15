451b33859f6176c6e23bae8caf466a5bead3bfce
Grant Sherrick <agsherrick@gmail.com>
2014-04-15 12:29:23 -0600

    Modified existing placeholder to use full page images and better layout.

--- PowdrMap/media/place_holder.js
+++ PowdrMap/media/place_holder.js
@@ -1,6 +1,26 @@
 ; $(document).ready(function(){
+
+    
+    /*var pages = ["#article1", "#article2", "#article3"],
+    pN = pages.length,
+    a = 0; // array position
+
+
+    $('article').on("wheel WheelEvent", function(e){
+    
+	var delta = parseInt(e.originalEvent.deltaX || -e.originalEvent.detail, 10);
+	
+	delta<0 ? ++a : --a;
+	a = a<0 ? pN-1 : a%pN; // loop pages array
+	
+	//if the mouse is scrolled, scroll the page to that link's hash
+	$.scrollTo( pages[a] || 0, 1500 );
+	e.preventDefault();
+    
+    });*/
+
     //this code is executed after the DOM is completely loaded, 
-    //  use the immediately indiced function experessions to do otherwise.
+    //  use the immediately induced function experessions to do otherwise.
     $('nav a, footer a.up').click(function(e){
 	
 	//if a link is clicked somewhere scroll the page to that link's hash
