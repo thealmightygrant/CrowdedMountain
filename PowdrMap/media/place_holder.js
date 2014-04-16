4dbf12939056b3e516a047a9c15d48aff49f20a2
Grant Sherrick <agsherrick@gmail.com>
2014-04-16 13:12:44 -0600

    Added ScrollTo javascript support for scrolling and clicking of links

--- PowdrMap/media/place_holder.js
+++ PowdrMap/media/place_holder.js
@@ -1,30 +1,59 @@
 ; $(document).ready(function(){
 
     
-    /*var pages = ["#article1", "#article2", "#article3"],
+    var pages = ["#article1", "#article2", "#article3"],
     pN = pages.length,
+    scrollingNow = false,
     a = 0; // array position
 
 
-    $('article').on("wheel WheelEvent", function(e){
-    
-	var delta = parseInt(e.originalEvent.deltaX || -e.originalEvent.detail, 10);
+    $(window).on("wheel WheelEvent", function(e){
 	
-	delta<0 ? ++a : --a;
-	a = a<0 ? pN-1 : a%pN; // loop pages array
-	
-	//if the mouse is scrolled, scroll the page to that link's hash
-	$.scrollTo( pages[a] || 0, 1500 );
+	if(scrollingNow === false){
+	    scrollingNow = true;
+
+	    var delta = parseInt(e.originalEvent.deltaY);
+	    
+	    delta < 0 ? --a : ++a;
+	    if (a >= pN ) {
+		a = (pN - 1);
+	    }
+	    if ( a < 0 ) {
+		a = 0;
+	    }
+
+	    //if the mouse is scrolled, scroll the page to that link's hash
+	    $( pages[a] ).ScrollTo({
+		duration: 1000,
+		callback: function(){
+		    scrollingNow = false;
+		}
+	    });
+	}
 	e.preventDefault();
-    
+	
+    });
+
+    //Code to change the format of the nav section depending on the section being presented
+    /*$('article').hover(
+	function(e) {
+	    //$("a[href='" +  +"']").addClass("in_view");
+	}, function(e) {
+	    $( this ).find( "span:last" ).remove();
+	}
     });*/
 
     //this code is executed after the DOM is completely loaded, 
-    //  use the immediately induced function experessions to do otherwise.
+    //  use the immediately induced function expressions to do otherwise.
     $('nav a, footer a.up').click(function(e){
 	
 	//if a link is clicked somewhere scroll the page to that link's hash
-	$.scrollTo( this.hash || 0, 1500 );
+	$( this.hash ).ScrollTo({
+	    duration: 1000
+	    //onlyIfOutside: true
+	    //easing: 'linear'
+	});
+
 	e.preventDefault();
     });
 });
