5eea5ed4b416d4546f2ba76039bfbcd4e1e9344c
Grant Sherrick <agsherrick@gmail.com>
2014-04-16 15:11:57 -0600

    Scrolling and Clicking works separately now, but not together

--- PowdrMap/media/place_holder.js
+++ PowdrMap/media/place_holder.js
@@ -1,11 +1,13 @@
 ; $(document).ready(function(){
 
-    
+    //this code is executed after the DOM is completely loaded, 
+    //  use the immediately induced function expressions to do otherwise.
+
     var pages = ["#article1", "#article2", "#article3"],
     pN = pages.length,
     scrollingNow = false,
+    lasta = -1,
     a = 0; // array position
-
 
     $(window).on("wheel WheelEvent", function(e){
 	
@@ -14,6 +16,7 @@
 
 	    var delta = parseInt(e.originalEvent.deltaY);
 	    
+	    lasta = a;
 	    delta < 0 ? --a : ++a;
 	    if (a >= pN ) {
 		a = (pN - 1);
@@ -27,33 +30,35 @@
 		duration: 1000,
 		callback: function(){
 		    scrollingNow = false;
+		    //call nav change function
+		    if (lasta != a){
+			$('#nav_' + lasta).addClass('in_view');
+			$('#nav_' + a).removeClass('in_view');
+		    }
 		}
 	    });
 	}
 	e.preventDefault();
 	
     });
+    
+    //if a link is clicked somewhere scroll the page to that link's hash
+    $('nav a').click(function(e){
+	if(scrollingNow == false){
+	    var link_name = $(this).attr("href");
+	    $( link_name ).ScrollTo({
+		duration: 1000,
+		callback: function(){
+		    scrollingNow = false;
+		    //call nav change function
+		    $('a[href=' + link_name + ']').addClass('in_view');
+		    $('a').not('[href=' + link_name + ']').removeClass('in_view');
 
-    //Code to change the format of the nav section depending on the section being presented
-    /*$('article').hover(
-	function(e) {
-	    //$("a[href='" +  +"']").addClass("in_view");
-	}, function(e) {
-	    $( this ).find( "span:last" ).remove();
+		    //set a
+		}
+	    });
+
+	    e.preventDefault();
 	}
-    });*/
-
-    //this code is executed after the DOM is completely loaded, 
-    //  use the immediately induced function expressions to do otherwise.
-    $('nav a, footer a.up').click(function(e){
-	
-	//if a link is clicked somewhere scroll the page to that link's hash
-	$( this.hash ).ScrollTo({
-	    duration: 1000
-	    //onlyIfOutside: true
-	    //easing: 'linear'
-	});
-
-	e.preventDefault();
     });
 });
